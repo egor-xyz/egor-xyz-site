f68c3a563230d8849a9fd3f71c43a0fd5ccc076b
Egor Stronhin <i@egor.xyz>
2024-01-26 22:18:51 +0200

    feat: animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,42 @@
-import { useState } from 'react';
+import { Variants, useAnimate } from 'framer-motion';
+import { useEffect } from 'react';
 import Logo from './assets/logo.svg?react';
-import { motion, Variants } from 'framer-motion';
 
 const variants: Variants = {
   start: {
     scale: [0, 1],
     rotate: [0, 360, 0],
-    opacity: [0, 1],
-    transition: { duration: 2 }
+    opacity: [0, 1]
   },
   top: {
     opacity: 1,
-    left: '5px',
-    top: '5px',
+    left: 15,
+    top: 15,
     scale: 0.4,
-    transformOrigin: 'top left',
-    transition: { duration: 0.7 }
+    transformOrigin: 'top left'
   }
 };
 
 export const App = () => {
-  const [logoVariant, setLogoVariant] = useState<'start' | 'top'>('start');
-  const toggleLogo = () => setLogoVariant('top');
+  const [scope, animate] = useAnimate();
+
+  const animateLogo = async () => {
+    await animate(scope.current, variants.start, { duration: 1.3 });
+    await animate(scope.current, variants.top, { duration: 0.7 });
+  };
+
+  useEffect(() => {
+    animateLogo();
+  }, []);
 
   return (
     <div className='text-100px relative flex min-h-[100svh] items-center justify-center bg-black bg-gradient-to-t from-blue-600 to-red-500 text-white'>
-      <motion.div
-        className='absolute opacity-0'
-        animate={logoVariant}
-        variants={variants}
-        onClick={toggleLogo}
+      <div
+        ref={scope}
+        className='absolute cursor-pointer opacity-0'
       >
         <Logo />
-      </motion.div>
+      </div>
     </div>
   );
 };
