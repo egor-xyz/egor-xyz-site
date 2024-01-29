96b29c3535fb4216de8b2f5c0f7e7624151e9a67
Egor Stronhin <i@egor.xyz>
2024-01-29 16:49:33 +0200

    cards

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,5 +1,9 @@
 import { FC } from 'react';
 
 export const Card: FC<{ children?: JSX.Element | string }> = ({ children = '' }) => {
-  return <div className='h-[400px] w-[600px] rounded-xl bg-white p-2 text-black shadow-xl'>{children}</div>;
+  return (
+    <div className='flex h-[400px] w-[600px] items-center justify-center rounded-xl bg-white p-2 text-2xl text-black shadow-xl'>
+      {children}
+    </div>
+  );
 };
