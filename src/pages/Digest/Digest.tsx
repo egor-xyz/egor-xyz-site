2a97d4f26e940886883b3d7f8e51813902ad1586
egor-xyz <i@egor.xyz>
2024-02-06 15:30:26 +0200

    feat: drops

--- src/pages/Digest/Digest.tsx
+++ src/pages/Digest/Digest.tsx
@@ -5,9 +5,6 @@ import { A, a } from 'src/animations/a';
 import cover from 'src/assets/digest-cover.jpg';
 import { Card } from 'src/components/Card';
 import { EnterAnimation } from 'src/components/EnterAnimation';
-import Drop from 'src/assets/drop-1.svg?react';
-import Drop2 from 'src/assets/drop-2.svg?react';
-import Drop3 from 'src/assets/drop-3.svg?react';
 
 const fadeInVariants: A = {
   enter: {
@@ -23,23 +20,6 @@ const fadeInVariants: A = {
     opacity: 0,
     scale: 0.4,
     y: 100
-  }
-};
-
-const dropAnimation: A = {
-  enter: {
-    opacity: 1,
-    scale: [0, 2, 1],
-    transition: {
-      delay: 1.5,
-      duration: 1,
-      type: 'spring'
-    }
-  },
-  initial: {
-    opacity: 0,
-    scale: 0.3,
-    transformOrigin: 'center center'
   }
 };
 
@@ -73,27 +53,6 @@ export const Digest = () => {
           >
             <FaLinkedin className='text-blue-500' /> Read on LinkedIn
           </a>
-        </motion.div>
-
-        <motion.div
-          {...a(dropAnimation)}
-          className='pointer-events-none absolute bottom-2 right-2'
-        >
-          <Drop className='h-[90px]' />
-        </motion.div>
-
-        <motion.div
-          {...a(dropAnimation)}
-          className='pointer-events-none absolute bottom-5 left-2'
-        >
-          <Drop2 className='h-[90px]' />
-        </motion.div>
-
-        <motion.div
-          {...a(dropAnimation)}
-          className='pointer-events-none absolute right-3 top-6'
-        >
-          <Drop3 className='h-[90px]' />
         </motion.div>
       </Card>
     </EnterAnimation>
