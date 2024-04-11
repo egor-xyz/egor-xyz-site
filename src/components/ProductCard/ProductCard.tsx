4c419bce1490e1c598f53701ba89e83de8d7e22e
Egor Stronhin <i@egor.xyz>
2024-04-11 11:48:40 +0300

    feat: Update extensions page

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,4 +1,23 @@
 import { FC, SVGProps } from 'react';
+import { Variants, motion } from 'framer-motion';
+import { a } from 'src/utils/a';
+
+const fadeInVariants: Variants = {
+  enter: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      delay: 0.5,
+      duration: 1
+    },
+    y: 0
+  },
+  initial: {
+    opacity: 0,
+    scale: 0.4,
+    y: 100
+  }
+};
 
 type Props = {
   Icon: FC<
@@ -13,11 +32,12 @@ type Props = {
 
 export const ProductCard: FC<Props> = ({ Icon, text, href, description }) => {
   return (
-    <a
+    <motion.a
       className='relative flex max-w-[90svw] items-center justify-items-start overflow-hidden rounded-xl bg-white text-base text-black shadow-xl md:text-xl'
       href={href}
       rel='noreferrer'
       target='_blank'
+      {...a(fadeInVariants)}
     >
       <div className='bg-black bg-gradient-to-t from-blue-600 to-red-500 p-2'>
         <Icon className='h-[100px] w-[100px]' />
@@ -30,6 +50,6 @@ export const ProductCard: FC<Props> = ({ Icon, text, href, description }) => {
         <div className='text-[#0099FF]'>{text}</div>
         <div className='max-w-[370px] pt-1 text-sm md:pt-2 md:text-base'>{description}</div>
       </div>
-    </a>
+    </motion.a>
   );
 };
--- src/pages/Extensions/Extensions.tsx
+++ src/pages/Extensions/Extensions.tsx
@@ -1,35 +1,15 @@
-import { Variants, motion } from 'framer-motion';
 import { TbBrandNpm } from 'react-icons/tb';
 import FMEPLogo from 'src/assets/fmep-logo.svg?react';
 import TailwindPackLogo from 'src/assets/tailwind-pack-logo.svg?react';
 import { ProductCard } from 'src/components/ProductCard';
-import { a } from 'src/utils/a';
+import { Card } from 'src/components/Card';
 
 import { EnterAnimation } from '../../components/EnterAnimation/EnterAnimation';
 
-const fadeInVariants: Variants = {
-  enter: {
-    opacity: 1,
-    scale: 1,
-    transition: {
-      delay: 0.5,
-      duration: 1
-    },
-    y: 0
-  },
-  initial: {
-    opacity: 0,
-    scale: 0.4,
-    y: 100
-  }
-};
 export const Extensions = () => {
   return (
     <EnterAnimation>
-      <motion.div
-        className='relative mt-5 flex w-full flex-col justify-start gap-8 text-lg'
-        {...a(fadeInVariants)}
-      >
+        <Card title='Extensions'>
         <ProductCard
           description='Make your Visual Studio Code the best IDE for the Front End developer'
           href='https://marketplace.visualstudio.com/items?itemName=egor-xyz.front-end-mega-extension-pack'
@@ -50,7 +30,8 @@ export const Extensions = () => {
           Icon={TbBrandNpm}
           text='ESLint configuration with hundreds of rules'
         />
-      </motion.div>
+        </Card>
+
     </EnterAnimation>
   );
 };
