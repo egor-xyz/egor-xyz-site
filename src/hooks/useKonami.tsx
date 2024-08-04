460db6b2ddfcb10fdc1130e085b3e1cb32bc9e86
Egor Stronhin <i@egor.xyz>
2024-08-04 21:04:27 +0300

    feat: konami code

--- src/hooks/useKonami.tsx
+++ src/hooks/useKonami.tsx
@@ -3,33 +3,28 @@ import { useCallback, useEffect, useRef, useState } from 'react';
 const konami = 'ArrowUpArrowUpArrowDownArrowDownArrowLeftArrowRightArrowLeftArrowRightba';
 
 export const useKonami = () => {
-  const keys = useRef<string[]>([]);
+  const keys = useRef<string>('');
   const [isKonami, setIsKonami] = useState(false);
 
   const handleKeyDown = useCallback(
     (e: KeyboardEvent) => {
       if (e.key === 'Escape') {
         console.log(e.key);
-        keys.current = [];
+        keys.current = '';
         setIsKonami(false);
         return;
       }
 
-      keys.current.push(e.key);
+      keys.current = keys.current + e.key;
 
-      const key = keys.current.join('');
-
-      console.log(key);
-      console.log(konami);
-
-      if (key.length > konami.length) {
-        keys.current = [];
+      if (keys.current.length > konami.length) {
+        keys.current = '';
         return;
       }
 
-      if (key === konami) {
+      if (keys.current === konami) {
         setIsKonami(true);
-        keys.current = [];
+        keys.current = '';
       }
     },
     [keys, isKonami]
