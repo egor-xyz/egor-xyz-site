75844209c00719e8d213b74ec1a38622e738148d
Egor Stronhin <i@egor.xyz>
2024-11-02 14:08:19 +0200

    fix: fix animation

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,7 +3,7 @@ import { MenuItem } from 'src/components/MenuItem';
 import { menuItems } from 'src/utils/menuItems';
 
 export const Home = () => (
-  <section className='mx-auto mt-[60px] w-[100%] max-w-5xl overflow-hidden p-14 pl-[20px] md:ml-[300px] md:mt-[80px] md:pl-[4px]'>
+  <section className='mx-auto mt-[60px] w-[100%] max-w-5xl p-14 pl-[20px] md:ml-[300px] md:mt-[80px] md:pl-[4px]'>
     <Macbook />
 
     {menuItems.map(({ heading, href, imgSrc, subheading }, key) => (
