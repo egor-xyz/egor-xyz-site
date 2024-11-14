c5ea335b132c0f91256e56aec0211c6e10d8d574
Egor Stronhin <i@egor.xyz>
2024-11-14 21:31:31 +0200

    fix: checkbox

--- src/components/Backgrounds/Backgrounds.tsx
+++ src/components/Backgrounds/Backgrounds.tsx
@@ -30,7 +30,7 @@ export const Backgrounds: FC = () => {
           className='peer hidden'
           id='toggle'
           type='checkbox'
-          onChange={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+          onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
         />
         <div className="relative h-[30px] w-[60px] rounded-full bg-white from-orange-500 to-yellow-400 opacity-30 shadow-sm duration-300 after:absolute after:left-[5px] after:top-[5px] after:h-[20px] after:w-[20px] after:rounded-full after:bg-gradient-to-r after:shadow-md after:duration-300 after:content-[''] active:after:w-[50px] peer-checked:bg-zinc-500 peer-checked:after:left-[55px] peer-checked:after:translate-x-[-100%] peer-checked:after:from-zinc-900 peer-checked:after:to-zinc-900" />
       </label>
